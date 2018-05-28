6b1c4305aaa7a9f0e7cb7252b175d80ccf1b4911
Calin <calin.nicolau@gmail.com>
2018-05-28 18:53:08 +0000

    Refactor

--- ex2.py
+++ ex2.py
@@ -1,11 +1,6 @@
 from scapy.all import *
 
-pdst = '198.13.13.0/16'
-eth = Ether(dst='ff:ff:ff:ff:ff:ff')
-arp = ARP(pdst=pdst) #Destination ip
-ans, unans = srp(eth/arp, timeout=1)
-
+ans, unans = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst='198.13.13.0/16'), timeout=1)
 for pkt in ans:
-    #print(pkt)
     print(pkt[1].psrc, '---', pkt[1].hwsrc)
 
